17e98f45886a6cbba51bc7e95d27412d34bb5b43
coldter <kuldeepparmar333@gmail.com>
2021-08-09 20:25:14 +0530

    added 6th definition and added ending div tag in some files and related changes

--- assignment-1/src/6/index.php
+++ assignment-1/src/6/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>DateInterva</title>
+	<title>OOPs Calculator </title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
@@ -30,21 +30,40 @@
 
 	<div class="container-contact100">
 		<div class="wrap-contact100">
-			<form action="" method="POST">
+			<form action="" method="POST" class="contact100-form">
 				<span class="contact100-form-title">
-				Date-Interva Calculator
+					OOPs Calculator
 				</span>
 
-				<label class="label-input100" for="date1">Enter First Date</label>
-				<div class="wrap-input100">
-					<input id="date1" class="input100" type="date" name="date1" required="required" placeholder="mm-dd-yyyy">
+				<label class="label-input100" for="fvalue">Enter Your Values*</label>
+				<div class="wrap-input100 rs1 validate-input">
+					<input id="fvalue" class="input100" type="text" name="fvalue" placeholder="First Value" required>
 					<span class="focus-input100"></span>
 				</div>
-
-				<label class="label-input100" for="date2">Enter Second Date</label>
-				<div class="wrap-input100">
-					<input id="date2" class="input100" type="date" name="date2" required="required" placeholder="mm-dd-yyyy">
+				<div class="wrap-input100 rs1 validate-input">
+					<input class="input100" type="text" name="svalue" placeholder="Second Value" required>
 					<span class="focus-input100"></span>
+				</div>
+				<div>	
+					<label class="label-input100" for="fvalue">Select Your Operation Choose*</label>
+					<ul>
+							<li>
+								<input id="r1" type="radio" name="opp" value="add" checked>
+								<label for="r1">Addition (+)</label>
+							</li>
+							<li>
+								<input id="r2" type="radio" name="opp" value="sub">
+								<label for="r2">Subtraction (-)</label>
+							</li>
+							<li>
+								<input id="r3" type="radio" name="opp" value="multi">
+								<label for="r3">Multiplication (*)</label>
+							</li>
+							<li>
+								<input id="r4" type="radio" name="opp" value="div">
+								<label for="r4">Division (/)</label>
+							</li>
+					</ul>
 				</div>
 
 				<div class="container-contact100-form-btn">
@@ -61,14 +80,75 @@
 																	php
 				 ------------------------------------------------------------------>
 				<?php
+				
+				set_error_handler(function () {
+					throw new Exception('BOOM!');
+				});
+				
+				
+				class MyCalculator{
+					private $a;
+					private $b;
 
-					if(!empty($_POST['date1']) && !empty($_POST['date2'])){
-						$date1 = new DateTime($_POST['date1']);
-						$date2 = new DateTime($_POST['date2']);
+					public function __construct($fvalue,$svalue)
+					{
+						$this->a = $fvalue;						
+						$this->b = $svalue;						
+					}
 
-						$interval = $date1->diff($date2);
+					public function add()
+					{
+						echo "<hr>", $this->a," + ", $this->b," = <b class=\"name\">", $this->a + $this->b, "</b>";
+					}
 
-						echo "<hr> Difference:  <b class=\"name\">", $interval->y, "</b> years, <b class=\"name\">", $interval->m, "</b> months, <b class=\"name\">", $interval->d, "</b> days.";
+					public function sub()
+					{
+						echo "<hr>", $this->a," - ", $this->b," = <b class=\"name\">", $this->a - $this->b, "</b>";
+					}
+
+					public function multi()
+					{
+						echo "<hr>", $this->a," * ", $this->b," = <b class=\"name\">", $this->a * $this->b, "</b>";
+					}
+
+					public function div()
+					{
+						try {
+							$result = $this->a / $this->b;
+							echo "<hr>", $this->a," / ", $this->b," = <b class=\"name\">", $result, "</b>";
+						}
+						catch(Exception $e){
+							echo "<hr> Undefined: Division by zero";
+						}
+					}
+				}
+
+					if(isset($_POST['opp']) && isset($_POST['fvalue']) && isset($_POST['svalue'])){
+						
+						if(!is_numeric($_POST['fvalue']) || !is_numeric($_POST['svalue'])){
+							echo "Enter Numeric Values only";
+							exit;
+						}
+						
+						$mycal = new MyCalculator($_POST['fvalue'], $_POST['svalue']);
+
+						switch($_POST['opp']){
+						case 'add':
+							$mycal->add();
+							break;
+						case 'sub':
+							$mycal->sub();
+							break;
+						case 'multi':
+							$mycal->multi();
+							break;
+						case 'div':
+							$mycal->div();
+							break;
+						default:
+							echo "Hacker-Man";
+							break;
+						}
 					}
 				?>
 				<!------------------------------------------------------------------
